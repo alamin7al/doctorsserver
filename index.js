96abf3e4e891b5e07678c981ec02fc3b425ca731
alamin <ma7029582@gmail.com>
2021-12-13 16:21:37 +0600

    hello

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://doctodb:qGqCa5bn3Y57LEdY@cluster0.ow5x2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ow5x2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
